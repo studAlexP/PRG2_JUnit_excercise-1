b357dcea8acdb47eb56182b30d1a4daa4be96fea
studAlexP <alexander.pruka@stud.fh-campuswien.ac.at>
2021-05-15 20:36:27 +0200

    implemented length checker, case checker, number checker and special char checker + test cases

--- src/main/java/passwordChecker/PasswordChecker.java
+++ src/main/java/passwordChecker/PasswordChecker.java
@@ -1,4 +1,8 @@
 package passwordChecker;
 
 public class PasswordChecker {
+
+    boolean checkPassword(String password) {
+        return password.matches("^(?=.{8,25})(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[()#$?!%/@]).+$");
+    }
 }
--- src/test/java/passwordChecker/PasswordCheckerTest.java
+++ src/test/java/passwordChecker/PasswordCheckerTest.java
@@ -1,4 +1,88 @@
 package passwordChecker;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PasswordCheckerTest {
+    @Test
+    @DisplayName("Password too short")
+    public void passwordLengthTest_Scenario1() {
+        PasswordChecker pw = new PasswordChecker();
+        boolean actual = pw.checkPassword("test");
+        boolean expected = false;
+        assertEquals(expected, actual);
+    }
+    @Test
+    @DisplayName("Password too long")
+    public void passwordLengthTest_Scenario2() {
+        PasswordChecker pw = new PasswordChecker();
+        boolean actual = pw.checkPassword("reallySecurePassword123456");
+        boolean expected = false;
+        assertEquals(expected, actual);
+    }
+    @Test
+    @DisplayName("Password length is valid")
+    public void passwordLengthTest_Scenario3() {
+        PasswordChecker pw = new PasswordChecker();
+        boolean actual = pw.checkPassword("Testojd()wIHsq23");
+        boolean expected = true;
+        assertEquals(expected, actual);
+    }
+    @Test
+    @DisplayName("Password needs atleast one big letter")
+    public void passwordLetterTest_Scenario1() {
+        PasswordChecker pw = new PasswordChecker();
+        boolean actual = pw.checkPassword("testabcdrt");
+        boolean expected = false;
+        assertEquals(expected, actual);
+    }
+    @Test
+    @DisplayName("Password needs atleast one small letter")
+    public void passwordLetterTest_Scenario2() {
+        PasswordChecker pw = new PasswordChecker();
+        boolean actual = pw.checkPassword("TESTABCDAV");
+        boolean expected = false;
+        assertEquals(expected, actual);
+    }
+    @Test
+    @DisplayName("Password letters are valid")
+    public void passwordLetterTest_Scenario3() {
+        PasswordChecker pw = new PasswordChecker();
+        boolean actual = pw.checkPassword("TeStAbc@232D");
+        boolean expected = true;
+        assertEquals(expected, actual);
+    }
+    @Test
+    @DisplayName("Password has no numbers")
+    public void passwordNumberTest_Scenario1() {
+        PasswordChecker pw = new PasswordChecker();
+        boolean actual = pw.checkPassword("firtwpclsfq");
+        boolean expected = false;
+        assertEquals(expected, actual);
+    }
+    @Test
+    @DisplayName("Password has numbers")
+    public void passwordNumberTestTest_Scenario2() {
+        PasswordChecker pw = new PasswordChecker();
+        boolean actual = pw.checkPassword("eR23#8r(en98");
+        boolean expected = true;
+        assertEquals(expected, actual);
+    }
+    @Test
+    @DisplayName("Password has no special characters")
+    public void passwordSpecialCharTest_Scenario1() {
+        PasswordChecker pw = new PasswordChecker();
+        boolean actual = pw.checkPassword("2ewdUHSgzo78dsu");
+        boolean expected = false;
+        assertEquals(expected, actual);
+    }
+    @Test
+    @DisplayName("Password has special characters")
+    public void passwordSpecialCharTest_Scenario2() {
+        PasswordChecker pw = new PasswordChecker();
+        boolean actual = pw.checkPassword("suhwSaYqw88p+#w");
+        boolean expected = true;
+        assertEquals(expected, actual);
+    }
 }
